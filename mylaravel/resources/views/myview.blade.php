92b8d9064863783ff3cbec3a064110568e465159
Thanusin leenarat <164463161+lilsunnoi@users.noreply.github.com>
2025-01-08 18:20:34 +0700

    test

---
+++ mylaravel/resources/views/myview.blade.php
@@ -0,0 +1,9 @@
+<h1>My Controller</h1>
+<h1>myinput: {{ $myinput; }}</h1>
+<h1>myvalue: {{ $myvalue; }}</h1>
+
+<form method="post" action="{{ url('/mycontroller/0') }}">
+    @csrf
+    <input type="text" name="myinput">
+    <button type="submit">Submit</button>
+</form>
