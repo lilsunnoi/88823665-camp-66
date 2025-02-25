cd128f4b3156ad94da4f84db506a6d69770f0ceb
Thanusin leenarat <164463161+lilsunnoi@users.noreply.github.com>
2025-02-25 22:06:56 +0700

    Reapply "not save"
    This reverts commit 42cc6a497a5ceeb59e4bb0953b3248c1683dec90.

--- mylaravel/app/Models/Category.php
+++ mylaravel/app/Models/Category.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'categories'; // กำหนดชื่อตาราง
-
-    public function products()
-    {
-        return $this->hasMany(ProductList::class, 'category_id');
-    }
+    public $timestamps = false;
+    protected $table = 'categories';
 }
--- mylaravel/app/Models/ProductList.php
+++ mylaravel/app/Models/ProductList.php
@@ -6,13 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductList extends Model
 {
-    protected $table = 'product_list'; // กำหนดชื่อตาราง
+    public $timestamps = false;
+    protected $table = 'product_list';
 
+    // ความสัมพันธ์กับตาราง categories
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    // ความสัมพันธ์กับตาราง users (แก้จาก users เป็น user)
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
