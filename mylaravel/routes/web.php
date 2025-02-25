57ebc706928f287af9927991c947e324cacf4fc0
Thanusin leenarat <164463161+lilsunnoi@users.noreply.github.com>
2025-02-26 00:48:03 +0700

    Update web.php

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,38 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\MyController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
-// Authentication Routes
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::post('/product', [ProductController::class, 'add_product'])->name('product.add');
+Route::post('/users/delete', [UserController::class, 'delete'])->name('users.delete');
+Route::get('/users', [UserController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/register', [RegisterController::class, 'index'])->name('register'); // Added route name
 Route::post('/register', [RegisterController::class, 'create']);
-
-// Public Routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('product', [ProductController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
-
-// Product Routes
-Route::get('/product', [ProductController::class, 'index'])->name('products.index');
-Route::post('/products/insert', [ProductController::class, 'insert'])->name('insert'); // Add this
-
-// User Routes
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
 Route::post('/users/edit', [UserController::class, 'edit_action'])->name('users.update');
-Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
-// Error Page
+Route::get('/mycontroller/{id?}', [MyController::class, 'myfunction']);
+Route::post('/mycontroller/{id?}', [MyController::class, 'MYFUNCTION']);
 Route::get('/error500', function () {
     abort(500, 'Internal Server Error');
 });
+Route::get('/user', function () {
+    return view('users');
+});
+Route::get('/hello/{id?}', function ($val = "") {
+    return "<h1>Hello World $val</h1>";
+});
 
-
-
+Route::post('/insert', [ProductController::class, 'insert'])->name('insert');
